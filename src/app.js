ac0110d4c326fe22982489d9c9dcb88e9f77d4fb
Maksym Kucher <the.stewk@gmail.com>
2019-06-29 20:05:48 -0700

    - Improved styling and added some script for starting the app.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Forecast',
         name: 'Max Kucher'
     })
 });
